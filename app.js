29d77e3902456edbdbed084a2400da03fc57b899
PedroAbramo <pedro.dalmeida@sptech.school>
2025-05-23 15:06:35 -0300

    Versão funcional 23/05/25

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -27,6 +26,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+
 
 
 app.listen(PORTA_APP, function () {
