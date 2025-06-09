2476868cf1c3f28197042b4449167d5dd7b1d8b2
PedroAbramo <pedro.dalmeida@sptech.school>
2025-06-08 22:54:12 -0300

    Adicionando dashboard na área administrativa

--- src/models/administrativoModel.js
+++ src/models/administrativoModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function buscarQuantidadeUsuarios() {
     var instrucao = `
-        SELECT DATE(data_criacao) AS dia, count(*) 
+        SELECT DATE(data_criacao) AS dia, count(*) AS quantidade 
         FROM usuario 
         GROUP BY DATE(data_criacao) 
         ORDER BY dia asc;
